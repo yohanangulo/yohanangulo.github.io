c6106c01c8fd8b17552a08dff1ba6ee3f6d7516d
yohanangulo <anguloyohan98@gmail.com>
2026-01-11 12:08:03 -0400

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [{ hostname: 'lh3.googleusercontent.com' }],
   },
 }
