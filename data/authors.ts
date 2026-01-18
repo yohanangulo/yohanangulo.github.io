db3683d4b10198fe5be6eee6506e1fe0471ce797
yohanangulo <anguloyohan98@gmail.com>
2026-01-18 17:12:23 -0400

    Create standalone author

--- data/authors.ts
+++ data/authors.ts
@@ -1,6 +1,10 @@
 import { Author } from './types'
 
+export const yohanAuthor: Author = {
+  name: 'Yohan Angulo',
+  url: 'https://github.com/yohanangulo',
+}
 export const defaultAuthors: Author[] | undefined = [
   { name: 'Cristian Murillo', url: 'https://github.com/cristianmgm7' },
-  { name: 'Yohan Angulo', url: 'https://github.com/yohanangulo' },
+  yohanAuthor,
 ]
